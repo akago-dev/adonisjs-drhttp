2c9438786947fb7be62e2aaa9d59970456b9e4e1
P.O Marec <po@marec.me>
2025-06-19 16:55:02 +0200

    Fix licence header

--- configure.ts
+++ configure.ts
@@ -1,7 +1,7 @@
 /*
- * @rlanz/sentry
+ * adonisjs-drhttp
  *
- * (c) Romain Lanz
+ * (c) AKAGO SAS <po@akago.fr>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
